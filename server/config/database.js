177773685f8b6d76c68c48a03082975ea79fb4ed
Sejanbagani1402 <sejanbagani14@gmail.com>
2025-07-19 16:45:15 +0530

    fix: db deprecated warnings

--- server/config/database.js
+++ server/config/database.js
@@ -3,11 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const con = await mongoose.connect(
-      process.env.MONGODB_URI || "mongodb://localhost:27107/shopie",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      process.env.MONGODB_URI || "mongodb://localhost:27107/shopie"
     );
 
     console.log(`Mongo db connected: ${con.connection.host}`);
